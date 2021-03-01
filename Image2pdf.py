648e887fb9e7c3f888d28d74e6e42c19122823a3
zain-ali-12 <63121360+zain-ali-12@users.noreply.github.com>
2021-03-01 14:46:36 +0400

    Update Image2pdf.py

--- Image2pdf.py
+++ Image2pdf.py
@@ -11,19 +11,29 @@ imgList = filedialog.askopenfilenames()
 imgList = list(imgList)
 
 print("This is the order your files will be converted to pdf in :")
-
+    
 for image in imgList:
-    print(image)
+    print(imgList.index(image) + 1, image)
+
+if input("\nIs the order correct? (Y/n): ") == "n":
+    try:
+        correct_order = input("\nEnter correct order or images separated by commas. eg: 2,3,1,5,4: ").split(",")
+    except Exception as e:
+        print("Please enter order correcctly")
+        correct_order = input("\nEnter correct order or images separated by commas. eg: 2,3,1,5,4: ").split(",")
+    corrected_list = []
+    for index in correct_order:
+        print(index)
+        corrected_list.append(imgList[int(index) - 1])
+    print(corrected_list)
+
 
 try:
     pdfData = img2pdf.convert(imgList)
+    fileName = input("Enter pdf filename: ")
+    with open(f"{fileName}.pdf", "wb") as pdffile:
+        pdffile.write(pdfData)
 
+    print("PDF Created!")
 except Exception as e:
-    print("Image has transparency which is not supported.")
-
-fileName = input("Enter pdf filename: ")
-
-with open(f"{fileName}.pdf", "wb") as pdffile:
-    pdffile.write(pdfData)
-
-print("PDF Created!")
+    print("Filetype not supported.")

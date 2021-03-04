0e3c2293f8ff8dae613f878d2a87f76e94b8b69c
zain-ali-12 <63121360+zain-ali-12@users.noreply.github.com>
2021-03-04 23:04:08 +0400

    Update Image2pdf.py

--- Image2pdf.py
+++ Image2pdf.py
@@ -1,28 +1,44 @@
 import time
 import tkinter as tk
 from tkinter import filedialog
-import os
+import subprocess
 import random
+
+import subprocess
+try:
+    subprocess.run(['wrongcommand'], check = True)
+except subprocess.CalledProcessError:
+    print ('wrongcommand does not exist')
+
+try: 
+    import colorama
+    from colorama import Fore, Back, Style
+except:
+    print("Requirements not found\n\nInstalling requirements now...\n")
+    try:
+        subprocess.run(['pip install colorama'], check = True)
+    except:
+        subprocess.run(['pip3 install colorama'], check = True)
+    print("\nRequirements have been installed. Run the applicaiton again.\n")
+    exit()
 
 try: 
     import img2pdf
 except:
     print("Requirements not found\n\nInstalling requirements now...\n")
     try:
-        os.system("apt install pip")
-        os.system("pip3 install img2pdf")
+        subprocess.run(['pip install img2pdf'], check = True)
     except:
-        try:
-            os.system("pip3 install img2pdf")
-        except:
-            os.system("pip2 install img2pdf")
-    print("\nRequirements have been installed. Run the applicaiton again.\n")
+        subprocess.run(['pip3 install img2pdf'], check = True)
+    print(f"\n{Fore.GREEN}[+]{Fore.RESET} Requirements have been installed. Run the applicaiton again.\n")
     exit()
 
+colorama.init(autoreset=True)
+
 try:
-    os.system("cls")
+    subprocess.run(['cls'], check = True)
 except:
-    os.system("clear")
+    subprocess.run(['clear'], check = True)
 
 banners = [
 r'''
@@ -62,39 +78,48 @@ root.withdraw()
 imgList = filedialog.askopenfilenames()
 
 if imgList == None or imgList == "" or imgList == []:
-    print("No files selected.\n\nExiting now...\n")
+    print(f"{Fore.RED}[+]{Fore.RESET} No files selected.\n\nExiting now...\n")
     time.sleep(2)
     exit()
 
 imgList = list(imgList)
-print("This is the order your files will be converted to pdf in :")
+print("[+] This is the order your files will be converted to pdf in :")
     
 for image in imgList:
     print(imgList.index(image) + 1, image)
 
-if input("\nIs the order correct? (Y/n): ") == "n":
+if input("\n[+] Is the order correct? (Y/n): ") == "n":
     try:
-        correct_order = input("\nEnter correct order or images separated by commas. eg: 2,3,1,5,4: ").split(",")
-    except Exception as e:
-        print("Please enter order correcctly")
-        correct_order = input("\nEnter correct order or images separated by commas. eg: 2,3,1,5,4: ").split(",")
-    corrected_list = []
-    for index in correct_order:
-        print(index)
-        corrected_list.append(imgList[int(index) - 1])
-    print(corrected_list)
+        correct_order = input("\n[+]Enter correct order of images separated by commas. eg: 2,3,1,5,4: ").split(",")
+    except:
+        print(f"{Fore.RED}[+]{Fore.RESET} Please enter order correcctly")
+        correct_order = input("\n[+] Enter correct order or images separated by commas. eg: 2,3,1,5,4: ").split(",")
 
+    corrected_list = []
+    try:
+        for index in correct_order:
+            corrected_list.append(imgList[int(index) - 1])
+    except:
+        print(f"{Fore.RED}[+]{Fore.RESET} Please enter order correcctly")
+        correct_order = input("\n[+] Enter correct order or images separated by commas. eg: 2,3,1,5,4: ").split(",")
+
+    print("[+] This is the order your files will be converted to pdf in :")
+    
+    for image in imgList:
+        print(imgList.index(image) + 1, image)
 
 try:
     pdfData = img2pdf.convert(imgList)
-    fileName = input("\nEnter pdf filename: ")
+    fileName = input("\n[+] Enter pdf filename: ")
+
     with open(f"{fileName}.pdf", "wb") as pdffile:
         pdffile.write(pdfData)
 
-    print("\nPDF Created!")
+    print(f"\n{Fore.GREEN}[+]{Fore.RESET} PDF Created!")
     print("\nExiting now...")
     time.sleep(2)
+
 except Exception as e:
-    print("\nFiletype not supported.")
+    print(f"\n{Fore.RED}[+]{Fore.RESET} Filetype not supported.")
     time.sleep(2)
     
